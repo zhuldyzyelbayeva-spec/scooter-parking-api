fb85b55275bf4500857904dd0b5b6e80ea2f9ea9
zhuldyzyelbayeva-spec <zhuldyzyelbayeva@gmail.com>
2026-02-09 14:23:08 +0500

    Delete main directory

--- main/java/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.endterm.scooterparkingapi.repository;
-
-import com.endterm.scooterparkingapi.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
